000a99818611b8c80ca5e97b36a822b27e86d2aa
xavierlu <xllgms@gmail.com>
2016-10-03 16:10:32 -0400

    init repo

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player") {
+			PlayerManager.SwitchDirection ();
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+	public static float speed = 7;
+	public static float spinSpeed = 7;
+	public GameObject sprite;
+	
+	void Update () {
+		if (Input.GetMouseButtonUp (0))
+			SwitchDirection ();
+		transform.Translate (Vector2.up * speed * Time.deltaTime);
+		sprite.transform.Rotate (Vector3.forward * spinSpeed);
+	}
+
+	public static void SwitchDirection(){
+		speed *= -1;
+		spinSpeed *= -1;
+	}
+}
