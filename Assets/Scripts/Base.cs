5cfad2b49e7d7547bb079d0e1079b14e3f2d8a53
Elizabeth Petrov <elizabeth.petrov@gmail.com>
2016-10-21 20:25:31 -0400

    New backgrounds, started background class

--- Assets/Scripts/Base.cs
+++ Assets/Scripts/Base.cs
@@ -7,13 +7,26 @@ public class Base : MonoBehaviour {
 	public static bool flag;//true = top; false = bottom
 	public static event Action OnPlayerSwitchDirection;
 	public static GameManager gameManager;
+	public Sprite notBent;
+	public Sprite bentUp;
+	public Sprite bentDown;
+	private SpriteRenderer sr;
 
 	void Start(){
 		gameManager = FindObjectOfType<GameManager> ();
 		flag = true;
+		sr = GetComponent<SpriteRenderer>();
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
+		if(this.gameObject.tag.Equals("BaseBottom"))
+		{
+			sr.sprite = bentDown;
+		}
+		else if(this.gameObject.tag.Equals("BaseTop"))
+		{
+			sr.sprite = bentUp;
+		}
 		if (other.tag == "Player") {
 			if (OnPlayerSwitchDirection != null)
 				OnPlayerSwitchDirection ();
@@ -22,5 +35,10 @@ public class Base : MonoBehaviour {
 				flag = !flag;
 			}
 		}
+	}
+
+	void onTriggerExit()
+	{
+		sr.sprite = notBent;
 	}
 }
